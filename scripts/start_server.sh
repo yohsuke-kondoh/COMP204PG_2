f993809e867c1c8a69e541f98609ec7d0320d34f
William Lam <zcabwhy@ucl.ac.uk>
2016-11-03 23:05:01 +0000

    cm

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /var/www/rails/sample
-mkdir /tmp/pids/
+cd /var/www/rails/sample/
+mkdir /var/www/rails/sample/tmp/pids/
 bundle exec unicorn -D -E production -c config/unicorn.conf.rb
