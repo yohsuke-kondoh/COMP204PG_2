618d3286403f39eb8a2c58bf539cbc9318db7f2c
William Lam <zcabwhy@ucl.ac.uk>
2016-11-03 23:17:10 +0000

    changes

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 cd /var/www/rails/sample/
-mkdir /var/www/rails/sample/tmp/pids/
 bundle exec unicorn -D -E production -c config/unicorn.conf.rb
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo chown -R ec2-user:ec2-user * /var/www/rails/sample
 ipd=`sudo cat /var/www/rails/sample/tmp/pids/unicorn.pid`
-sudo kill -KILL -s QUIT  $ipd
+sudo kill -QUIT  $ipd
