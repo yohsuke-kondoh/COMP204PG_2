517a7558a160698d7bcde97b588b398982c6d3c4
William Lam <zcabwhy@ucl.ac.uk>
2016-11-03 22:36:10 +0000

    change stuff

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo chown -R ec2-user:ec2-user * /var/www/rails/sample
-cd /var/www/rails/sample && bundle exec unicorn -D -E production -c config/unicorn.conf.rb
+cd /var/www/rails/sample
+sudo bundle exec unicorn -D -E production -c config/unicorn.conf.rb
