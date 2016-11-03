5ccefa4d4df29a46918526d4082e8630c7a890af
William Lam <zcabwhy@ucl.ac.uk>
2016-11-03 23:54:50 +0000

    c

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-touch /temp2/who
+mkdir /var/www/rails/sample/tmp/pids
+mkdir /var/www/rails/sample/tmp/sockets
 cd /var/www/rails/sample/
 bundle exec unicorn -D -E production -c config/unicorn.conf.rb
