6fe84daa30904c8bf8b13ba0011bb1e6b6cdb17e
kazuma <zcabkho@ucl.ac.uk>
2016-11-03 18:36:25 +0000

    mod start server

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-su -l deploy -c 'cd /var/www/rails/sample && bundle exec unicorn -D -E production -c config/unicorn.rb'
+cd /var/www/rails/sample && bundle exec unicorn -D -E production -c config/unicorn.rb
