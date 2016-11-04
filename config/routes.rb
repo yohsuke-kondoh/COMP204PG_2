a3b123e35c80fdec4402176128f1e83d9da1aec4
Asim Ali <zcablix@ucl.ac.uk>
2016-11-04 11:37:48 +0000

    improved interface

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :bookmarks
   devise_for :users
-  get 'home/index'
-  root 'home#index'
+  get 'bookmarks/index'
+  root 'bookmarks#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
